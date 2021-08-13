87631e40d798c0ab8ad9d275474a5d6178347de8
Katie Stapleton <Deepdarkstar7@gmail.com>
2021-08-12 23:54:42 -0500

    simple response added when user clicks on say hello button

--- app/src/main/java/com/zybooks/uiuxandroid_katies/MainActivity.java
+++ app/src/main/java/com/zybooks/uiuxandroid_katies/MainActivity.java
@@ -1,8 +1,12 @@
 package com.zybooks.uiuxandroid_katies;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sayHello(View view) {
+        TextView outputText = findViewById(R.id.textGreeting);
+        EditText inputText = findViewById(R.id.nameText);
+        Button button = findViewById(R.id.buttonSayHello);
+
+        String sayHelloMessage = (getString(R.string.welcomeGreeting) + inputText.getText());
+        button.setOnClickListener(view1 -> outputText.setText(sayHelloMessage));
     }
 }
