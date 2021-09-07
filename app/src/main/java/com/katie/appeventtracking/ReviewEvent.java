e11ec7bd929fac881c63ce22a3e11170a435e3b3
Katie Stapleton <Deepdarkstar7@gmail.com>
2021-09-06 19:26:02 -0500

    added new activities. manage events allows users to enter and submit new events. uses SQLite. not yet directed to manageevents, but return button should work. XML is currentl scroll/linear, consider changing to constraint late.

---
+++ app/src/main/java/com/katie/appeventtracking/ReviewEvent.java
@@ -0,0 +1,14 @@
+package com.katie.appeventtracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReviewEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_event);
+    }
+}
